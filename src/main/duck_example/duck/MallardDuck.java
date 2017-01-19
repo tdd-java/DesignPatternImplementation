5fa115bb9e48830fab027647f44ea6877992a87a
Priti Biyani <pritib@in-pritibiyani.local>
2017-01-19 13:58:29 +0530

    Basic Duck skeleton is added

---
+++ src/main/duck_example/duck/MallardDuck.java
@@ -0,0 +1,16 @@
+package duck_example.duck;
+
+public class MallardDuck {
+
+  public String quack() {
+    return "Quack! Quack!";
+  }
+
+  public String swim() {
+    return "I can Swim!";
+  }
+
+  public String display() {
+    return "I'm Mallard Duck!";
+  }
+}
---
+++ src/test/duck_example/duck/MallardDuckTest.java
@@ -0,0 +1,31 @@
+package duck_example.duck;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MallardDuckTest {
+  MallardDuck mallardDuck;
+
+  @Before
+  public void setUp() throws Exception {
+    mallardDuck = new MallardDuck();
+  }
+
+  @Test
+  public void shouldBeAbleToQuack() {
+    assertThat(mallardDuck.quack(), is( "Quack! Quack!"));
+  }
+
+  @Test
+  public void shouldBeAbleToSwim()  {
+    assertThat(mallardDuck.swim(), is("I can Swim!"));
+  }
+
+  @Test
+  public void shouldDisplayMallardDuckLogo()  {
+    assertThat(mallardDuck.display(), is("I'm Mallard Duck!"));
+  }
+}
